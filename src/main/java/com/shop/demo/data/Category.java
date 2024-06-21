209998de5ab1ab5f9ace8c4395706be895b8bf62
AndreaTamara1 <est_a_tamara@fesc.edu.co>
2024-06-21 14:39:40 -0500

    Created structure of data for the products

---
+++ src/main/java/com/shop/demo/data/Category.java
@@ -0,0 +1,5 @@
+package com.shop.demo.data;
+
+public enum Category {
+    ELECTRONICS, CLOTHING, FOOD, BOOKS
+}
---
+++ src/main/java/com/shop/demo/data/Products.java
@@ -0,0 +1,64 @@
+package com.shop.demo.data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Products {
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Price")
+    private String price;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Code")
+    private Integer code;
+
+    @Column(name = "Category")
+    private Category category;
+
+    @Column(name = "Stock")
+    private String stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+}

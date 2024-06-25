a17b1ae22b2ee7a41d1e4e16cb3b5f943e1bb145
Sebastián <sebbarrera@gmail.com>
2024-06-25 17:39:39 -0500

    Created repository and service

---
+++ src/main/java/com/shop/demo/service/ShopService.java
@@ -0,0 +1,17 @@
+package com.shop.demo.service;
+
+import com.shop.demo.data.Product;
+
+import java.util.List;
+
+public interface ShopService {
+    Product save(Product product);
+
+    void delete(Product product);
+
+    List<Product> search(String name);
+
+    List<Product> findAll();
+
+    Integer inventoryValue();
+}
